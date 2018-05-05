47c1de3bb7474ddf859a3bcd7c5519b231cf1fbe
Agustin Ramirez <38988940+AgustinR27@users.noreply.github.com>
2018-05-04 22:34:18 -0300

    Update TP_main.py

--- TP_main.py
+++ TP_main.py
@@ -1,7 +1,8 @@
-from texto import obtener_texto
+from TP_texto import obtener_texto
+import random
 
-def formatearPalabra(palabra):
-    dic_a_reemplazar = {"Ñ":"NI","Á":"A","É":"E","Í":"I","Ó":"O","Ú":"U"}
+def formatear_palabras(palabra):
+    dic_a_reemplazar = {"Ñ": "NI", "Á": "A", "É": "E", "Í": "I", "Ó": "O", "Ú": "U"}
     palabra_vieja = palabra.upper()
     palabra_nueva = ''
     for letra in palabra_vieja:
@@ -9,24 +10,32 @@ def formatearPalabra(palabra):
             palabra_nueva += letra
         else:
             palabra_nueva += dic_a_reemplazar[letra]
-
     return palabra_nueva
 
-def main():
-    variable = obtener_texto()
+
+def obtener_palabras():
+    texto = obtener_texto()
     dic_palabras = {}
-    for linea in variable:
+    for linea in texto:
         if len(linea) > 0:
             lista_aux = linea.split(" ")
             for palabra in lista_aux:
                 if palabra.isalpha() and len(palabra) >= 5:
-                    if formatearPalabra(palabra) not in dic_palabras:
-                        dic_palabras[formatearPalabra(palabra)] = 1
+                    if formatear_palabras(palabra) not in dic_palabras:
+                        dic_palabras[formatear_palabras(palabra)] = 1
                     else:
-                        dic_palabras[formatearPalabra(palabra)] += 1
+                        dic_palabras[formatear_palabras(palabra)] += 1
+    return dic_palabras
 
+
+def enlistar_palabras(dic_palabras):
+    lista_palabras = []
     for clave in dic_palabras:
-        print(clave)
+        if clave not in lista_palabras:
+            lista_palabras.append(clave)
+    return lista_palabras
 
-    print(len(dic_palabras))
-main()
+
+def palabra_adivinar(lista_palabras):
+    palabra_adivinar = random.choice(lista_palabras)
+    return palabra_adivinar
